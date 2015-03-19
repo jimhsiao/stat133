2d93e8580a2c662066d1fda4b9437c71b93406f7
Jim Hsiao <jimhsiao95@gmail.com>
2015-03-19 16:10:33 -0700

    homework 6

--- assignments/hw5/bml_simulation.R
+++ assignments/hw5/bml_simulation.R
@@ -20,5 +20,21 @@ bml.sim(10,10, 0.6)
 bml.sim(5,10,0.6)
 bml.sim(10,5,0.6)
 
+pdf(file = "bml_figures.pdf")
+#1
+barplot(matrix, beside= TRUE, names.arg = density2, xlab = "Density of Cars", ylab = "Gridlock Proportion", main= "Gridlock Rates of Different Car Densities", col = "purple")
+#2
+boxplot(my_matrix, main = "# Iterations Until Gridlock For Different Densities", xlab = "Density (in tenths)", ylab = "# Iterations", col = "orange")
+
+#3A
+barplot(matrix2, beside= TRUE, names.arg = dimensions, xlab = "Individual Side Dimension", ylab = "Gridlock Proportion", main= "Gridlock Rates of Different Sized (Square) Dimensions", col = "pink")
+#3b
+barplot(matrixShape, main = "Gridlock Rates of Square and Rectangle Grids", beside = TRUE, xlab = "Dimension of Grid", ylab = "Gridlock Rate", names.arg = c("5x5", "4x6", "10x10", "4x25"))
+boxplot(square, main = "# Iterations Until Gridlock For Small 5x5 Square Grid", xlab = "Density", ylab = "# Iterations", col = "blue")
+boxplot(rect, main = "# Iterations Until Gridlock For Small 4x6 Uneven Grid", xlab = "Density (in tenths)", ylab = "# Iterations", col = "yellow")
+boxplot(big_square, main = "# Iterations Until Gridlock For Big 10x10 Square Grid", xlab = "Density (in tenths)", ylab = "# Iterations", col = "green")
+boxplot(big_rect, main = "# Iterations Until Gridlock For Big 4x25 Uneven Grid", xlab = "Density (in tenths)", ylab = "# Iterations", col = "red")
+dev.off()
+
 
 
--- hw6/hw6.r
+++ hw6/hw6.r
@@ -38,6 +38,36 @@ sim.doctors <- function(initial.doctors, n.doctors, n.days, p){
   # 3) convert the non-adopter with probability p
 
   # return the output
+  initial.doctors = matrix(c(initial.doctors), nrow = n.doctors, ncol = 1)
+  has_adopted <- matrix(nrow = n.doctors, ncol = n.days - 1, 0)
+  has_adopted <- cbind(initial.doctors, has_adopted)
+  for(j in 1:n.doctors) {
+    if(has_adopted[j, 1] == 1) {
+      for(k in 1:n.days) {
+        has_adopted[j, k] = 1
+      }
+    }
+  }
+  for(i in 1:n.days) {
+    random <- sample(1:n.doctors, 2)
+    if(has_adopted[random[1], i] == 0 && has_adopted[random[2], i] == 1) {
+      toChange <- sample(1:2, size = 1, prob = c(p, 1-p))
+      if(toChange ==  1) {
+        for(x in i:n.days) {
+          has_adopted[random[1], x] = 1
+        }
+      }
+    } else if(has_adopted[random[1], i] == 1 && has_adopted[random[2], i] == 0) {
+      toChange <- sample(1:2, size = 1, prob = c(p, 1-p))
+      if(toChange ==  1) {
+        for(x in i:n.days) {
+          has_adopted[random[2], x] = 1
+        }
+      }
+    }
+  }
+  
+  return(has_adopted)
 
 }
 
@@ -45,9 +75,39 @@ sim.doctors <- function(initial.doctors, n.doctors, n.days, p){
 # pick values for the other input parameters.
 
 set.seed(42)
+
+count.doctors <- function(end.matrix) {
+  perday <- vector()
+  for(i in 1:ncol(end.matrix)) {
+    tempSum = 0
+    for(j in 1:nrow(end.matrix)) {
+      if(end.matrix[j,i] == 1) {
+        tempSum <- tempSum + 1
+      }
+    }
+    perday <- c(perday, tempSum)
+  }
+  return(perday)
+}
+
 # Generate a value for <initial.doctors> that has 10% 1s and 90% 0s.
 # Run your function for at least 5 different values of <p> and plot
 # on x-axis: days,
 # on y-axis : the number of doctors that have already adopted the drug, on that day
 # Put all 5 lines in one figure (e.g. use first plot() then lines() for the subsequent lines)
+initDoctors = c(1, rep(0, 9), 1, rep(0, 9))
+first.sim <- sim.doctors(initDoctors, 20, 20, 1)
+initDoctors = c(1, rep(0, 9), 1, rep(0, 9))
+second.sim <- sim.doctors(initDoctors, 20, 20, 0.8)
+initDoctors = c(1, rep(0, 9), 1, rep(0, 9))
+third.sim <- sim.doctors(initDoctors, 20, 20, 0.5)
+initDoctors = c(1, rep(0, 9), 1, rep(0, 9))
+fourth.sim <- sim.doctors(initDoctors, 20, 20, 0.3)
+initDoctors = c(1, rep(0, 9), 1, rep(0, 9))
+fifth.sim <- sim.doctors(initDoctors, 20, 20, 0.1)
 
+plot(count.doctors(first.sim), type = "o", xlab = "days", ylab = "numer of doctors adopted", main = "Drug adoption simulation")
+lines(count.doctors(second.sim), type = "o", col = "blue")
+lines(count.doctors(third.sim), type = "o", col = "red")
+lines(count.doctors(fourth.sim), type = "o", col = "green")
+lines(count.doctors(fifth.sim), type = "o", col = "yellow")
